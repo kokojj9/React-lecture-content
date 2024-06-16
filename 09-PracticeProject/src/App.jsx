db43e5f75700261a3d79c2ce3c51fa9f1b37ad74
kokojj <kovjscl976@naver.com>
2024-06-16 21:17:37 +0900

    예제 프로젝트

--- 09-PracticeProject/src/App.jsx
+++ 09-PracticeProject/src/App.jsx
@@ -20,20 +20,21 @@ function App() {
 
   const handleAddProject = projectData => {
     setProjectState(prevState => {
+      const projectId = Math.random();
       const newProject = {
         ...projectData,
-        id: Math.random()
+        id: projectId
       };
 
       return {
         ...prevState,
+        selectedProjectId : undefined,
         projects: [...prevState.projects, newProject]
       };
     })
   };
 
-  console.log(projectState);
-
+  
   let content;
 
   if(projectState.selectedProjectId === null) {
@@ -46,7 +47,10 @@ function App() {
 
   return (
     <main className="h-screen my-8 flex gap-8"> 
-      <ProjectsSidebar onStartAddProject={handleStartAddProject} />
+      <ProjectsSidebar 
+        onStartAddProject={handleStartAddProject} 
+        projects={projectState.projects} 
+      />
       {content}
     </main>
   );
