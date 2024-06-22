56a0d594a0ab434e47b83ba171844073c9db1209
kokojj <kovjscl976@naver.com>
2024-06-22 15:29:02 +0900

    useReducer에 대해

--- 10-AdvancedStateManagement/src/store/shopping-cart-context.jsx
+++ 10-AdvancedStateManagement/src/store/shopping-cart-context.jsx
@@ -1,12 +1,105 @@
-import { createContext } from 'react';
+import { createContext, useState, useReducer } from 'react';
+
+import { DUMMY_PRODUCTS } from '../dummy-products';
 
 export const CartContext = createContext({
     items: [],
-    addItemToCart: () => {}, // 초기에 값들을 설정은 해줘야 새로 추가되어도 인식할 수 있음 강의에서는 이 함수를 나중에 추가했음
-    updateItemQuantity: () => {}
+    addItemToCart: () => { }, // 초기에 값들을 설정은 해줘야 새로 추가되어도 인식할 수 있음 강의에서는 이 함수를 나중에 추가했음
+    updateItemQuantity: () => { }
 }); //의도적으로 대문자로 상수변수를 사용했음 컴포넌트로 사용 할 것이기 때문에
 
+function shoppingCartReducer(state, action) {
+    if(action.type === 'ADD_ITEM'){
+            const updatedItems = [...state.items];
 
+            const existingCartItemIndex = updatedItems.findIndex(
+                (cartItem) => cartItem.id === action.payload
+            );
+            const existingCartItem = updatedItems[existingCartItemIndex];
+
+            if (existingCartItem) {
+                const updatedItem = {
+                    ...existingCartItem,
+                    quantity: existingCartItem.quantity + 1,
+                };
+                updatedItems[existingCartItemIndex] = updatedItem;
+            } else {
+                const product = DUMMY_PRODUCTS.find((product) => product.id === action.payload);
+                updatedItems.push({
+                    id: action.payload,
+                    name: product.title,
+                    price: product.price,
+                    quantity: 1,
+                });
+            }
+
+            return {
+                ...state, // 복잡한 프로젝트일 경우 데이터를 잃지않도록 깊은 복사를 해주는게 좋다
+                items: updatedItems,
+            };
+    }
+
+    return state;
+}
+
+
+
+export default function CartContextProvider({ children }) {
+    const [ shoppingCartState, shoppingCartDispatch ] =  useReducer(
+        shoppingCartReducer, 
+        {
+            items: [],
+        }
+    );
+
+    const [shoppingCart, setShoppingCart] = useState({
+        items: [],
+    });
+
+    function handleAddItemToCart(id) {
+        shoppingCartDispatch({
+            type: 'ADD_ITEM',
+            payload: id
+        });
+    }
+
+    function handleUpdateCartItemQuantity(productId, amount) {
+        setShoppingCart((prevShoppingCart) => {
+            const updatedItems = [...prevShoppingCart.items];
+            const updatedItemIndex = updatedItems.findIndex(
+                (item) => item.id === productId
+            );
+
+            const updatedItem = {
+                ...updatedItems[updatedItemIndex],
+            };
+
+            updatedItem.quantity += amount;
+
+            if (updatedItem.quantity <= 0) {
+                updatedItems.splice(updatedItemIndex, 1);
+            } else {
+                updatedItems[updatedItemIndex] = updatedItem;
+            }
+
+            return {
+                items: updatedItems,
+            };
+        });
+    }
+
+    const ctxValue = {
+        items: shoppingCartState.items,
+        addItemToCart: handleAddItemToCart,
+        updateItemQuantity: handleUpdateCartItemQuantity,
+    };
+
+    return (
+        <CartContext.Provider value={ctxValue}>
+            {children}
+        </CartContext.Provider>
+    );
+}
 
 
 
