e23836e1eb1e1e0b105468786578d4f387858a83
kokojj <kovjscl976@naver.com>
2024-07-08 11:50:01 +0900

    기본적인 리액트의 form태그 활용

--- 17-FormsUserInput/src/components/Login.jsx
+++ 17-FormsUserInput/src/components/Login.jsx
@@ -1,23 +1,63 @@
+import { useState } from "react";
+
 export default function Login() {
+  // const [enteredEmail, setEnteredEmail] = useState('');
+  // const [enteredPassword, setEnteredPassword] = useState('');
+  // 객체 state 를 만들어 하나로 통합해도 상관 없음
+  const [enteredValues, setEnteredValues] = useState({
+    email: '',
+    password: ''
+  });
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    console.log(enteredValues);
+  }
+
+  function handleInputChange(identifier, value) {
+    setEnteredValues(prevValues => ({
+      ...prevValues,
+      [identifier]: value
+      // e.terget.value가 아니라 value 를 바로 넘겨주면 됨
+      //여기서 [] 는 배열이 아니라 객체 속성값을 변수로 설정할 수 있는 내장 기능이다.
+    }));
+  }
+
+  function handlePasswordChange(e){
+    setEnteredValues(e.target.value);
+  }
+
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <h2>Login</h2>
 
       <div className="control-row">
         <div className="control no-margin">
           <label htmlFor="email">Email</label>
-          <input id="email" type="email" name="email" />
+          <input
+            id="email"
+            type="email"
+            name="email"
+            onChange={e => handleInputChange('email', e.target.value)}
+            value={enteredValues.email}
+          />
         </div>
 
         <div className="control no-margin">
           <label htmlFor="password">Password</label>
-          <input id="password" type="password" name="password" />
+          <input 
+            id="password" 
+            type="password" 
+            name="password" 
+            onChange={e => handleInputChange('password', e.target.value)}
+            value={enteredValues.password}
+          />
         </div>
       </div>
 
       <p className="form-actions">
         <button className="button button-flat">Reset</button>
-        <button className="button">Login</button>
+        <button className="button" onClick={handleSubmit}>Login</button>
       </p>
     </form>
   );
