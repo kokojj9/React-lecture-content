68222e342ebd00ebc0fefbbcc6e28bc9961dde61
kokojj <kovjscl976@naver.com>
2024-07-09 22:19:27 +0900

    음식주문 앱 실습

---
+++ 18-FoodOrder/src/components/Header.jsx
@@ -0,0 +1,16 @@
+import logoImg from '../assets/logo.jpg';
+
+export default function Header() {
+    return(
+        <header id="main-header">
+            <div id="title">
+                <img src={logoImg} alt='A logo Image'/>
+                <h1>ReactFood</h1>
+            </div>
+            <nav>
+                <button>Cart (0)</button>
+            </nav>
+        </header>
+    );
+
+}
