c4a753c1710eabcca57ca87f37e4bfe006dbda09
kokojj <kovjscl976@naver.com>
2024-07-16 16:58:04 +0900

    리덕스 기본 활용법

--- React-Complete-Guide/src/store/index.js
+++ React-Complete-Guide/src/store/index.js
@@ -1,46 +1,7 @@
 // import { createStore } from "redux";
-import { createSlice, configureStore } from "@reduxjs/toolkit";
-
-const initialCounterState = { counter: 0, showCounter: true };
-
-const counterSlice = createSlice({
-  name: "counter",
-  initialState: initialCounterState,
-  reducers: {
-    increment(state) {
-      state.counter++;
-      // toolkit createSlice는 기존 상태를 변경할 수 없기 때문에 이렇게 사용해도됨
-      // 내부적으로 알아서 변경할 수 없는 코드로 바꿔줌
-    },
-    decrement(state) {
-      state.counter--;
-    },
-    increase(state, action) {
-      state.counter = state.counter + action.payload;
-    },
-    toggleCounter(state) {
-      state.showCounter = !state.showCounter;
-    },
-  },
-});
-
-const initialAuthState = {
-  isAuthenticated: false,
-};
-
-const authSlice = createSlice({
-  name: "authentication",
-  initialState: initialAuthState,
-  reducers: {
-    login(state) {
-      state.isAuthenticated = true;
-    },
-    logout(state) {
-      state.isAuthenticated = false;
-    },
-  },
-});
-
+import { configureStore } from "@reduxjs/toolkit";
+import counterReducer from "./counter.js";
+import authReducer from './auth.js';
 // const counterReducer = (state = { counter: 0, showCounter: true }, action) => {
 //   // redux를 사용할때에는 기존의 state를 절대 변경해서는 안됨!!
 //   // 깊은 복사를 하거나 새 객체를 반환해야함!
@@ -82,9 +43,7 @@ const authSlice = createSlice({
 const store = configureStore({
   // 여러 리듀서를 병합해줄 수 있음
   // reducer: { counter: counterSlice.reducer } // 이런식으로
-  reducer: { counter: counterSlice.reducer, auth: authSlice.reducer },
+  reducer: { counter: counterReducer, auth: authReducer },
 });
 
-export const counterActions = counterSlice.actions;
-export const authActions = authSlice.actions;
 export default store;
