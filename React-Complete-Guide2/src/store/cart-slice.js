0f47487b1c5fdd367361b52794b5ea0fe58ef1e7
kokojj <kovjscl976@naver.com>
2024-07-17 18:05:44 +0900

    리덕스와 useEffect 사용하기

--- React-Complete-Guide2/src/store/cart-slice.js
+++ React-Complete-Guide2/src/store/cart-slice.js
@@ -7,6 +7,10 @@ const cartSlice = createSlice({
     totalQuantity: 0,
   },
   reducers: {
+    replaceCart(state, action) {
+      state.totalQuantity = action.payload.totalQuantity;
+      state.items = action.payload.items;
+    },
     addItemToCart(state, action) {
       const newItem = action.payload;
       const existingItem = state.items.find((item) => item.id === newItem.id);
@@ -23,6 +27,8 @@ const cartSlice = createSlice({
         existingItem.quantity++;
         existingItem.totalPrice = existingItem.totalPrice + newItem.price;
       }
+
+      //리듀서 안에서 비동기 코드를 실행 하면 안됨.
     },
     removeItemFromCart(state, action) {
       const id = action.payload;
