19eefddcd8a5868b7cc61c501051fb24f570d9d2
kokojj <kovjscl976@naver.com>
2024-07-19 19:28:01 +0900

    router 기존 사용법

--- 20-React-router/src/App.js
+++ 20-React-router/src/App.js
@@ -1,5 +1,35 @@
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from "react-router-dom";
+import HomePage from "./pages/Home";
+import ProductsPage from "./pages/Products";
+import RootLayout from "./pages/Root";
+
+// const routeDefinitions = createRoutesFromElements(
+//   <Route>
+//     <Route path="/" element={<HomePage />} />
+//     <Route path="/products" element={<ProductsPage />} />
+//   </Route>
+// );
+
+// const router = createBrowserRouter(routeDefinitions);
+// 위 아래 두가지 방법중 어느것이든 개발자의 취향임
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <RootLayout />,
+    children: [
+      { path: "/", element: <HomePage /> },
+      { path: "products", element: <ProductsPage /> },
+    ],
+  },
+]);
+
 function App() {
-  return <div></div>;
+  return <RouterProvider router={router} />;
 }
 
 export default App;
