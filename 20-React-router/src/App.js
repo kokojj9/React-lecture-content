6822fbc71c842e135158d4abf0406cad6f8623f5
kokojj <kovjscl976@naver.com>
2024-07-21 00:25:31 +0900

    절대경로와 상대경로

--- 20-React-router/src/App.js
+++ 20-React-router/src/App.js
@@ -25,9 +25,10 @@ const router = createBrowserRouter([
     element: <RootLayout />,
     errorElement: <ErrorPage />,
     children: [
-      { path: "/", element: <HomePage /> },
+      { path: "", element: <HomePage /> },
       { path: "products", element: <ProductsPage /> },
       { path: "products/:productId", element: <ProductDetailPage /> }, // 콜론기호로 동적url을 만들 수 있음 어떠한 값이 들어가도 엘리먼트 컴포넌트를 띄워줌
+      // path의 맨앞에 / 를 붙히면 절대경로, 없이 사용하면 상대경로로 자동으로 판단함
     ],
   },
 ]);
