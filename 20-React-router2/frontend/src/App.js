0505ca677ac6e844eb82805345f3ed4106ec55d6
kokojj <kovjscl976@naver.com>
2024-07-22 22:08:01 +0900

    라우터 활용 연습예제

--- 20-React-router2/frontend/src/App.js
+++ 20-React-router2/frontend/src/App.js
@@ -20,8 +20,24 @@
 // 7. Output the ID of the selected event on the EventDetailPage
 // BONUS: Add another (nested) layout route that adds the <EventNavigation> component above all /events... page components
 
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import HomePage from "./pages/Home";
+import EventsPage from "./pages/Events";
+import EventDetailPage from "./pages/EventDetail";
+import NewEventPage from "./pages/NewEvent";
+import EditEventPage from "./pages/EditEvent";
+
+const router = createBrowserRouter([
+  { path: "/", element: <HomePage /> },
+  { path: "/events", element: <EventsPage /> },
+  { path: "/events/:eventId", element: <EventDetailPage /> },
+  { path: "/events/new", element: <NewEventPage /> },
+  // 리액트에서는 이렇게 지정해도 /new 의 url경로가 /:eventId로 읽히지 않고 우선도를 가짐
+  { path: "events/:eventId/edit", element: <EditEventPage /> },
+]);
+
 function App() {
-  return <div></div>;
+  return <RouterProvider router={router}></RouterProvider>;
 }
 
 export default App;
