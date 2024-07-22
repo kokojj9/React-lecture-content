d33f2d10ad3fe9ce192304bf49b371bd0b49d7b6
kokojj <kovjscl976@naver.com>
2024-07-22 23:11:09 +0900

    커스텀 오류를 이용한 오류처리

--- 20-React-router2/frontend/src/pages/Events.js
+++ 20-React-router2/frontend/src/pages/Events.js
@@ -1,10 +1,15 @@
 import { useLoaderData } from "react-router";
 
 import EventsList from "../components/EventsList";
+import { useState } from "react";
 
 function EventsPage() {
   const data = useLoaderData(); // loader가 산출하는 최종 데이터를 받음
   const events = data.events;
+
+  if (data.isError) {
+    return <p>{data.message}</p>;
+  }
 
   return (
     <>
@@ -16,10 +21,13 @@ function EventsPage() {
 export default EventsPage;
 
 export async function loader() {
+  // loader는 리액트 컴포넌트가 아님! 훅을 사용할 수 없음
+
   const response = await fetch("http://localhost:8080/events");
 
   if (!response.ok) {
-    // ...
+    // return { isError: true, message: "Could not fetch events." };
+    throw { message: "Could not fetch events." };
   } else {
     // const resData = await response.json();
     // const res = new Response("any data", { status: 201 });
