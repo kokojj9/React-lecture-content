410e3c1c1555f4089d10a42236bd2077f37d5b7a
kokojj <kovjscl976@naver.com>
2024-07-24 00:48:31 +0900

    request()메서드로 액션 재사용하기

--- 20-React-router2/frontend/src/components/EventForm.js
+++ 20-React-router2/frontend/src/components/EventForm.js
@@ -1,8 +1,17 @@
-import { Form, useNavigate, useNavigation } from "react-router-dom";
+import {
+  json,
+  redirect,
+  Form,
+  useActionData,
+  useNavigate,
+  useNavigation,
+} from "react-router-dom";
 
 import classes from "./EventForm.module.css";
 
 function EventForm({ method, event }) {
+  const data = useActionData();
+  // 가장 가까운 action의 값을 반환
   const navigate = useNavigate();
   const navigation = useNavigation();
   const isSubmitting = navigation.state === "submitting";
@@ -12,7 +21,14 @@ function EventForm({ method, event }) {
   }
 
   return (
-    <Form method="post" className={classes.form}>
+    <Form method={method} className={classes.form}>
+      {data && data.errors && (
+        <ul>
+          {Object.values(data.errors).map((err) => (
+            <li key={err}>{err}</li>
+          ))}
+        </ul>
+      )}
       {/* Form으로 데이터를 제출하면 바로 백엔드로 가는것이 아니라 action()으로 보내짐 */}
       <p>
         <label htmlFor="title">Title</label>
@@ -67,3 +83,39 @@ function EventForm({ method, event }) {
 }
 
 export default EventForm;
+
+export async function action({ request, params }) {
+  const method = request.method;
+  const data = await request.formData();
+  const eventData = {
+    title: data.get("title"),
+    image: data.get("image"),
+    date: data.get("date"),
+    description: data.get("description"),
+  };
+
+  let url = "http://localhost:8081/events";
+  //request에는 모두 대문자로 포함 하고있기 때문에 대문자로
+  if (method === "PATCH") {
+    const eventId = params.eventId;
+    url = "http://localhost:8081/events/" + eventId;
+  }
+
+  const response = await fetch(url, {
+    method: method,
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(eventData),
+  });
+
+  if (!response.status === 422) {
+    return response;
+  }
+
+  if (!response.ok) {
+    throw json({ message: "Could not save event." }, { status: 500 });
+  }
+
+  return redirect("/events");
+}
