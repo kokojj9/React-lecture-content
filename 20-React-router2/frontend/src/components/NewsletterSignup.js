b9117174e4bbce51744328a3100a34caa446262e
kokojj <kovjscl976@naver.com>
2024-07-24 19:59:55 +0900

    useFetcher()를 이용한 배후 작업

---
+++ 20-React-router2/frontend/src/components/NewsletterSignup.js
@@ -0,0 +1,28 @@
+import { useFetcher, Form } from "react-router-dom";
+import classes from "./NewsletterSignup.module.css";
+import { useEffect } from "react";
+
+function NewsletterSignup() {
+  const fetcher = useFetcher();
+  const { data, state } = fetcher;
+  //fetcher 는 loader가 있는 페이지를 로딩하지않고 트리거하고싶을 때 사용함
+
+  useEffect(() => {
+    if (state === "idle" && data && data.message) {
+      window.alert(data.message);
+    }
+  }, [data, state]);
+
+  return (
+    <fetcher.Form method="post" className={classes.newsletter}>
+      <input
+        type="email"
+        placeholder="Sign up for newsletter..."
+        aria-label="Sign up for newsletter"
+      />
+      <button>Sign up</button>
+    </fetcher.Form>
+  );
+}
+
+export default NewsletterSignup;
