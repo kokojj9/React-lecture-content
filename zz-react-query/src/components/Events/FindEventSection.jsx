654db117a2fb4dc22ff7ea2e18130811c11c6e78
kokojj <kovjscl976@naver.com>
2024-08-02 22:39:09 +0900

    동적 쿼리 함수 및 쿼리 키

--- zz-react-query/src/components/Events/FindEventSection.jsx
+++ zz-react-query/src/components/Events/FindEventSection.jsx
@@ -1,10 +1,44 @@
-import { useRef } from 'react';
+import { useRef, useState } from "react";
+import { useQuery } from "@tanstack/react-query";
+import { fetchEvents } from "../../util/http.js";
+import EventItem from "./EventItem";
 
 export default function FindEventSection() {
   const searchElement = useRef();
+  const [searchTerm, setSerachTerm] = useState();
+
+  const { data, isPending, isError, error } = useQuery({
+    queryKey: ["events", { search: searchTerm }],
+    queryFn: () => fetchEvents(searchTerm),
+  });
+
+  let content = <p>Please enter a search term and to find events.</p>;
+
+  if (isPending) {
+    content = <LoadingIndicator />;
+  }
+
+  if (isError) {
+    content = (
+      <ErrorBlock title="An error occurred" message={error.info?.message} />
+    );
+  }
+
+  if (data) {
+    content = (
+      <ul className="events-list">
+        {data.map((event) => (
+          <li key={event.id}>
+            <EventItem event={event} />
+          </li>
+        ))}
+      </ul>
+    );
+  }
 
   function handleSubmit(event) {
     event.preventDefault();
+    setSerachTerm(searchElement.current.value);
   }
 
   return (
@@ -20,7 +54,7 @@ export default function FindEventSection() {
           <button>Search</button>
         </form>
       </header>
-      <p>Please enter a search term and to find events.</p>
+      {content}
     </section>
   );
 }
