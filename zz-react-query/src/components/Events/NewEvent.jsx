a392c73e6a2c1aea293909d6f78f82b89838388d
kokojj <kovjscl976@naver.com>
2024-08-03 21:30:37 +0900

    투가 데이터 가져오기 및 변형 테스트 추가하기

--- zz-react-query/src/components/Events/NewEvent.jsx
+++ zz-react-query/src/components/Events/NewEvent.jsx
@@ -1,25 +1,46 @@
-import { Link, useNavigate } from 'react-router-dom';
+import { Link, useNavigate } from "react-router-dom";
+import { useMutation } from "@tanstack/react-query";
 
-import Modal from '../UI/Modal.jsx';
-import EventForm from './EventForm.jsx';
+import Modal from "../UI/Modal.jsx";
+import EventForm from "./EventForm.jsx";
+import { createNewEvent } from "../../util/http.js";
+import ErrorBlock from "../UI/ErrorBlock.jsx";
 
 export default function NewEvent() {
   const navigate = useNavigate();
 
-  function handleSubmit(formData) {}
+  const { mutate, isPending, isError, error } = useMutation({
+    // 컨포넌트가 렌더링될때 자동으로 함수를 실행해주지 않음
+    mutationFn: createNewEvent,
+  });
+
+  function handleSubmit(formData) {
+    mutate({ event: formData });
+  }
 
   return (
-    <Modal onClose={() => navigate('../')}>
+    <Modal onClose={() => navigate("../")}>
       <EventForm onSubmit={handleSubmit}>
-        <>
-          <Link to="../" className="button-text">
-            Cancel
-          </Link>
-          <button type="submit" className="button">
-            Create
-          </button>
-        </>
+        {isPending && "Submitting..."}
+        {!isPending && (
+          <>
+            <Link to="../" className="button-text">
+              Cancel
+            </Link>
+            <button type="submit" className="button">
+              Create
+            </button>
+          </>
+        )}
       </EventForm>
+      {isError && (
+        <ErrorBlock
+          title="Failed to crate evnet"
+          message={
+            error.info?.message || "failed to create enevt. Please aaaa..."
+          }
+        />
+      )}
     </Modal>
   );
 }
