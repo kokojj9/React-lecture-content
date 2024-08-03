5d76a0c4cd961069d99242a926eeb33281a8f56a
kokojj <kovjscl976@naver.com>
2024-08-03 22:20:13 +0900

    useMutation에 대해

--- zz-react-query/src/components/Events/NewEvent.jsx
+++ zz-react-query/src/components/Events/NewEvent.jsx
@@ -3,7 +3,7 @@ import { useMutation } from "@tanstack/react-query";
 
 import Modal from "../UI/Modal.jsx";
 import EventForm from "./EventForm.jsx";
-import { createNewEvent } from "../../util/http.js";
+import { createNewEvent, queryClient } from "../../util/http.js";
 import ErrorBlock from "../UI/ErrorBlock.jsx";
 
 export default function NewEvent() {
@@ -12,6 +12,10 @@ export default function NewEvent() {
   const { mutate, isPending, isError, error } = useMutation({
     // 컨포넌트가 렌더링될때 자동으로 함수를 실행해주지 않음
     mutationFn: createNewEvent,
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ["events"] }); // 이 키를 사용하는 모든 쿼리가 무효화됨 
+      navigate("/events");
+    },
   });
 
   function handleSubmit(formData) {
