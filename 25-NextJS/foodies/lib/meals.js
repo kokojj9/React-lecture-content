a00fc61c6a0141a8797cade105e99eefc694f128
kokojj <kovjscl976@naver.com>
2024-08-09 21:01:03 +0900

    오류처리 방법

--- 25-NextJS/foodies/lib/meals.js
+++ 25-NextJS/foodies/lib/meals.js
@@ -4,6 +4,8 @@ const db = sql("meals.db");
 
 export async function getMeals() {
   await new Promise((resolve) => setTimeout(resolve, 5000));
-  return db.prepare("SELECT * FROM meals").all(); 
+
+  // throw new Error("Loading meals failed");
+  return db.prepare("SELECT * FROM meals").all();
   // run은 데이터를 추가하거나 수정할때 all은 가져올때
 }
