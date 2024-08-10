276ae4337339bd3b0091529605d4ab3e8283efcd
kokojj <kovjscl976@naver.com>
2024-08-10 22:39:45 +0900

    서버단 입력 유효성 확인 추가 방법

--- 25-NextJS/foodies/lib/action.js
+++ 25-NextJS/foodies/lib/action.js
@@ -1,10 +1,13 @@
-'use server';
+"use server";
 
 import { redirect } from "next/navigation";
 import { saveMeal } from "./meals";
 
+function isInvalidText(text) {
+  return !text || text.trim() === "";
+}
+
 export async function shareMeal(formData) {
-  
   const meal = {
     title: formData.get("title"),
     summary: formData.get("summary"),
@@ -14,6 +17,19 @@ export async function shareMeal(formData) {
     creator_email: formData.get("email"),
   };
 
+  if (
+    isInvalidText(meal.title) ||
+    isInvalidText(meal.summary) ||
+    isInvalidText(instructions) ||
+    isInvalidText(creator) ||
+    isInvalidText(creator_email) ||
+    !meal.creator_email.include("@") ||
+    !meal.image ||
+    meal.image.size === 0
+  ) {
+    throw new Error("Invalid input");
+  }
+
   await saveMeal(meal);
-  redirect('/meals');
+  redirect("/meals");
 }
