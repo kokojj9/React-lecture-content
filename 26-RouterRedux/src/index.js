4cf85cd8c32a0ecc501a05e1c24bf959a6587304
kokojj <kovjscl976@naver.com>
2024-08-12 18:43:05 +0900

    커스텀 저장소 사용하기

--- 26-RouterRedux/src/index.js
+++ 26-RouterRedux/src/index.js
@@ -4,13 +4,16 @@ import { BrowserRouter } from "react-router-dom";
 
 import "./index.css";
 import App from "./App";
-import ProductsProvider from "./context/products-context";
+// import ProductsProvider from "./context/products-context";
+import configureProductsStore from "./hooks-store/products-store";
+
+configureProductsStore();
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <ProductsProvider>
+  // <ProductsProvider>
     <BrowserRouter>
       <App />
     </BrowserRouter>
-  </ProductsProvider>
+  // </ProductsProvider>
 );
